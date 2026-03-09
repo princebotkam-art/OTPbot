9a86858ff90a2cee3b8ec3744452d0e4ba4394f9
princebotkam-art <princebotkam@gmail.com>
2026-03-09 13:06:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 # Bot configuration
-BOT_TOKEN = os.getenv('7991250024:AAF5Fy7nrm1rweCvLKU_--WVZqk-mIHMDU0')
-GROUP_ID = os.getenv('-1003377700516')
-IVASMS_EMAIL = os.getenv('abdullahalgeria@mailbox.in.ua')
-IVASMS_PASSWORD = os.getenv('@Abdullah1&')
+BOT_TOKEN = os.getenv('8073903913:AAHEZZDpvk4WiFjrig2h9LhAcB5Sy-JobUM')
+GROUP_ID = os.getenv('-1003868394296')
+IVASMS_EMAIL = os.getenv('ryder261649@maillii.website')
+IVASMS_PASSWORD = os.getenv('OMU BABY890')
 
 # Bot statistics
 bot_stats = {
